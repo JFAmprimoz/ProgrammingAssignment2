e76c7b38f41201092ea6d6d54415049d48be2b80
JFgit <jfusinggit@gmail.com>
2015-06-20 18:59:16 -0400

    Completed Intro to R Assignment 2
    Completed and commented required functions and functionality for
    assignment 2 on fork of rdpeng/programmingassignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions will take in (or create) a matrix, x,
+## establish a cache, then check the cache before calculating
+## the matrix inverse and updating the cache if necessary.
 
-## Write a short comment describing this function
-
+## This function creates a matrix, a cache, and a way to change
+## them using a list of four functions
 makeCacheMatrix <- function(x = matrix()) {
-
+     
+     mat <- NULL
+     set <- function(y) { ## set passes argument to matrix x
+          x <<- y
+          mat <<- NULL  ## ... and clears cache
+     }
+     
+     get <- function() x   ## return matrix to invert
+     setinverse <- function(inverse) mat <<- inverse ## write new inverse to cache
+     getinverse <- function() mat ## return cache
+     
+     list(set = set, get = get,   ## list of functions
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x' either from the cache
+## (if available) or by calculating it (in which case it also updates cache)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     
+     mat <- x$getinverse() ## load value from cache
+     
+     if(!is.null(mat)) {  # check if cache is null
+          message("getting cached data") 
+          return(mat)   ## if not, return the value in the cache, ending the function
+     }
+     
+     data <- x$get()  ## if so, function procedes by retrieving matrix x
+     mat <- solve(data, ...) ## finding its inverse
+     x$setinverse(mat) ## and writing it to the cache.
+     
+     mat  
 }
